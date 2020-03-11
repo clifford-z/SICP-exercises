e55585fe7b8b0ddbdf8246619a8e37f99201c4da
ikspike <clifford.k.zhang@outlook.com>
2020-03-11 20:54:28 +0800

    Finish add-change example, add some libs

---
+++ Chap.01/math.scm
@@ -0,0 +1,29 @@
+#lang racket
+;Sauare
+(define (square x)
+  (* x x))
+
+; Factorial, Iteration
+(define (factorial n)
+  (fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+      product
+      (fact-iter (* counter product)
+                 (+ counter 1)
+                 max-count)))
+
+; Exponent, Recursive
+(define (expt b n)
+  (if (= n 0)
+      1
+      (* b (expt b (- n 1)))))
+
+; Fast-Exponent
+(define (fast-expt b n)
+ (cond ((= n 0) 1)
+       ((even? n) (square (fast-expt b (/ n 2))))
+       (else (*b (fast-expt b (- n 1))))))
+
+(provide (all-defined-out))
