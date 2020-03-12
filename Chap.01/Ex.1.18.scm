cc14d0aa954a382e73c9645e4d015fa46570ba89
ikspike <clifford.k.zhang@outlook.com>
2020-03-12 20:14:12 +0800

    Fix the bug of Ex.1.18

--- Chap.01/Ex.1.18.scm
+++ Chap.01/Ex.1.18.scm
@@ -4,7 +4,7 @@
 
 (define (mult-iter a b product)
   (cond ((= b 0) product)
-        ((even? b) (mult-iter a (halve b) (double product)))
+        ((even? b) (mult-iter (double a) (halve b) product))
         (else (mult-iter a (- b 1) (+ product a)))))
 
 (define (even? n)
