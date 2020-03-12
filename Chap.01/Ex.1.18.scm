3a758271dd5b26c740fc2098702dd24d82b78e5d
ikspike <clifford.k.zhang@outlook.com>
2020-03-12 20:09:55 +0800

    Finish Ex.1.18 with a bug
    > (* 2 0)
    0
    > (* 2 2)
    2
    > (* 2 3)
    6
    > (* 2 4)
    2

---
+++ Chap.01/Ex.1.18.scm
@@ -0,0 +1,17 @@
+#lang sicp
+(define (* a b)
+  (mult-iter a b 0))
+
+(define (mult-iter a b product)
+  (cond ((= b 0) product)
+        ((even? b) (mult-iter a (halve b) (double product)))
+        (else (mult-iter a (- b 1) (+ product a)))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (double n)
+  (+ n n))
+
+(define (halve n)
+  (/ n 2))
