de10d82f1d91423b2871f82b268f7704e5916da0
ikspike <clifford.k.zhang@outlook.com>
2020-03-12 20:01:00 +0800

    Fix  the bug of Ex.1.17

--- Chap.01/Ex.1.17.scm
+++ Chap.01/Ex.1.17.scm
@@ -1,7 +1,7 @@
 #lang sicp
 (define (* a b)
   (cond ((= b 0) 0)
-        ((even? b) (+ (double a) (* a (halve b))))
+        ((even? b) (double (* a (halve b))))
         (else (+ a (* a (- b 1))))))
 
 (define (even? n)
