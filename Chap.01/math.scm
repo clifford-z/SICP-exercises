7cfedae8cc006b06bc2c9e58698aad7d1d5850f5
ikspike <clifford.k.zhang@outlook.com>
2020-03-30 11:28:45 +0800

    Add math functions

--- Chap.01/math.scm
+++ Chap.01/math.scm
@@ -1,5 +1,9 @@
 #lang racket
-;Sauare
+; Average
+(define (average a b)
+  (/ (+ a b) 2))
+
+; Square
 (define (square x)
   (* x x))
 
@@ -26,4 +30,83 @@
        ((even? n) (square (fast-expt b (/ n 2))))
        (else (* b (fast-expt b (- n 1))))))
 
+; Greatest common factor
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+; Find divisor
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= (smallest-divisor n) n))
+
+; Fermat test
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+; Half-interval roots finding
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+              (cond ((positive? test-value)
+                     (search f neg-point midpoint))
+                    ((negative? test-value)
+                     (search f midpoint pos-point))
+                    (else midpoint))))))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+           (error "values are not opposite sign" a b)))))
+
+; Fixed-point finding
+(define (fixed-point f first-guess)
+  (define tolerance 0.00001)
+  (define (close-enough v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+; Export package
 (provide (all-defined-out))
