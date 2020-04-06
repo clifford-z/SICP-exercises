ca859516038ed92e3162f7e2ca7ecf97b9fb3748
ikspike <clifford.k.zhang@outlook.com>
2020-04-06 15:31:15 +0800

    Refine math.scm

--- Collections/math/math.scm
+++ Collections/math/math.scm
@@ -10,6 +10,10 @@
 (define (square x)
   (* x x))
 
+; Devides?
+(define (divides? a b)
+  (= (remainder b a) 0))
+
 ; Factorial, Iteration
 (define (factorial n)
   (fact-iter 1 1 n))
@@ -40,19 +44,24 @@
       (gcd b (remainder a b))))
 
 ; Find divisor
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n)
+            n)
+          ((divides? test-divisor n)
+            test-divisor)
+          (else
+            (find-divisor n (next test-divisor)))))
+
 (define (smallest-divisor n)
   (find-divisor n 2))
 
-(define (find-divisor n test-divisor)
-  (cond ((> (square test-divisor) n) n)
-        ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (+ test-divisor 1)))))
-
-(define (divides? a b)
-  (= (remainder b a) 0))
-
 (define (prime? n)
   (= (smallest-divisor n) n))
+
+(define (next n)
+    (if (= n 2)
+        3
+        (+ n 2)))
 
 ; Fermat test
 (define (expmod base exp m)
