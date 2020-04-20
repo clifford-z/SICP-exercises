a54fd9fe29d2e599f12dd4b2f6e52aaaf3fa23e9
ikspike <clifford.k.zhang@outlook.com>
2020-04-21 00:24:49 +0800

    Finish Ex.1.27

---
+++ Chap.01/Ex.1.27.scm
@@ -0,0 +1,9 @@
+#lang sicp
+(#%require "../Collections/math/math.scm")
+
+(fast-prime? 561 5)
+(fast-prime? 1105 5)
+(fast-prime? 1729 5)
+(fast-prime? 2465 5)
+(fast-prime? 2821 5)
+(fast-prime? 6601 5)
