7f70575bbbf182319389fd54c7b3efb48671621c
ikspike <clifford.k.zhang@outlook.com>
2020-05-13 15:37:23 +0800

    Finish Ex.1.32 with a bug fix at line 7

--- Chap.01/Ex.1.32.scm
+++ Chap.01/Ex.1.32.scm
@@ -4,10 +4,15 @@
   (if (> a b)
       null-value
       (combiner (term a)
-                (accumulate combiner null-value term (term (next a)) next b))))
+                (accumulate combiner null-value term (next a) next b))))
 
+; Sum
 (define (sum term a next b)
   (accumulate + 0 term a next b))
+
+; Product
+(define (product term a next b)
+  (accumulate * 1.0 term a next b))
 
 ; Cube
 (define (cube x)
@@ -17,3 +22,15 @@
   (define (next a)
     (+ a 1))
   (sum cube a next b))
+
+(define (pi-product a b)
+  (define (term n)
+    (if (odd? n)
+        (/ (+ n 1) (+ n 2))
+        (/ (+ n 2) (+ n 1))))
+  (define (next a)
+    (+ a 1))
+  (product term a next b))
+
+(* 4 (pi-product 1 1000))
+(* 4 (pi-product 1 1000000))
