e83981f018b7ec87b8845ed630c5c02fae2666ca
ikspike <clifford.k.zhang@outlook.com>
2020-05-13 16:34:00 +0800

    Finish Ex.1.32b

--- Chap.01/Ex.1.32b.scm
+++ Chap.01/Ex.1.32b.scm
@@ -1,10 +1,11 @@
 #lang racket
-;Recursive
+; Iteration
 (define (accumulate combiner null-value term a next b)
-  (if (> a b)
-      null-value
-      (combiner (term a)
-                (accumulate combiner null-value term (next a) next b))))
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
 
 ; Sum
 (define (sum term a next b)
