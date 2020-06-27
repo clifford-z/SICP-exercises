11d6c3d2d418a5a0b8f83ef5bc45c96c731bb6e2
ikspike <clifford.k.zhang@outlook.com>
2020-06-28 01:22:52 +0800

    Finish Ex.2.28

---
+++ Chap.02/Ex.2.28.scm
@@ -0,0 +1,11 @@
+#lang racket
+; Enumerate leaves of a tree
+(define (fringe tree)
+  (cond ((null? tree) null)
+        ((not (pair? tree)) (list tree))
+        (else (append (fringe (car tree))
+                      (fringe (cdr tree))))))
+
+(define x (list (list 1 2) (list 3 4)))
+(fringe x)
+(fringe (list x x))
